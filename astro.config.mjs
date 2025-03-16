3bcf2663e0fe40009dc072c6c80d9b484ec9d73e
Gungcakra <agungcakra888@gmail.com>
2025-03-17 00:56:56 +0800

    Update .htaccess for client folder redirection and change Astro configuration output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,7 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'static', // ✅ Wajib gunakan 'static'
-  integrations: [react()],
-  build: {
-    format: 'directory', 
-  },
-  adapter: node({ mode: 'standalone' }),
+  output: 'server', // Pastikan output adalah server
+  adapter: node({ mode: 'standalone' }), // Tambahkan mode
   integrations: [react()],
 });
