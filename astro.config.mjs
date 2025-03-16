8e4f0f4b861c9306259ca049f01b45be53fb3253
Gungcakra <agungcakra888@gmail.com>
2025-03-17 00:52:57 +0800

    Remove .htaccess file and update Astro configuration to use static output and directory format for builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server', // Pastikan output adalah server
-  adapter: node({ mode: 'standalone' }), // Tambahkan mode
+  output: 'static', // ✅ Wajib gunakan 'static'
+  integrations: [react()],
+  build: {
+    format: 'directory', 
+  },
+  adapter: node({ mode: 'standalone' }),
   integrations: [react()],
 });
