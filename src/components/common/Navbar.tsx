aa391a153dba331cc11f75851815e42f9430d971
Gungcakra <agungcakra888@gmail.com>
2025-03-17 13:14:47 +0800

    Rename "Daftar Manhwa" to "Explore" in Navbar component

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
               { name: "Home", link: "/" },
               { name: "Bookmark", link: "/ongoing" },
               { name: "History", link: "/completed" },
-              { name: "Daftar Manhwa", link: "/anime" },
+              { name: "Explore", link: "/manhwa" },
             ].map((item, index) => (
               <li key={index} className="mb-4 md:mb-0">
                 <a
